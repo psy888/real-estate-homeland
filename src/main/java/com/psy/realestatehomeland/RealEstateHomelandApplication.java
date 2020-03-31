94dafd97dff9298c92c67bfc577963d484efcb5c
psy888 <tbilisi.zp.snab@gmail.com>
2020-03-31 23:09:22 +0300

    init

---
+++ src/main/java/com/psy/realestatehomeland/RealEstateHomelandApplication.java
@@ -0,0 +1,13 @@
+package com.psy.realestatehomeland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateHomelandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateHomelandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psy/realestatehomeland/RealEstateHomelandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psy.realestatehomeland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateHomelandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
