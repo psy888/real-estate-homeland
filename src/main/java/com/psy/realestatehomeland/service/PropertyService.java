6e2f3da7ce5335afed29b2acb824ecfc1f35ffa2
psy888 <tbilisi.zp.snab@gmail.com>
2020-04-14 14:01:12 +0300

    add java doc, fix update method

--- src/main/java/com/psy/realestatehomeland/service/PropertyService.java
+++ src/main/java/com/psy/realestatehomeland/service/PropertyService.java
@@ -16,22 +16,37 @@ public class PropertyService {
     private final PropertyRepository repository;
 
 
+    /**
+     * ["New York", 5]
+     *
+     * @return String[CityName, CountRepeatsInDb]
+     */
     public List<String[]> getCitiesWithCnt() {
         return repository.findAllCities();
     }
 
+    /**
+     * ["commercial", 1]
+     *
+     * @return String[propertyType, CountRepeatsInDb]
+     */
     public List<String[]> getPropertyTypes() {
         return repository.findAllPropertyTypes();
     }
 
-    public Property getPropertyById(String id) {
-        return repository.findById(id).orElse(new Property()); //???
-    }
 
     public List<Property> getPropertyByAgent(String email) {
         return repository.findAllByAgent_Email(email);
     }
 
+    /**
+     * Search property in db by params
+     *
+     * @param type   - propertyType  or "" (empty string by default)
+     * @param action - propertyAction or "" (empty string by default)
+     * @param city   - cityName or "" (empty string by default)
+     * @return list of found property
+     */
     public List<Property> searchProperty(String type, String action, String city) {
         return repository.findAllByPropertyTypeAndPropertyActionAndCity(
                 ((type.isEmpty()) ? null : type),
@@ -49,7 +64,7 @@ public class PropertyService {
     }
 
     public Property findById(String id) {
-        return repository.findById(id).orElse(null); //todo replace to throw
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Property not found with id:" + id));
     }
 
 
@@ -61,12 +76,26 @@ public class PropertyService {
         repository.save(property);
     }
 
+    /**
+     * Update all fields except id and agent
+     *
+     * @param id - property to update in db
+     * @param property - new property data
+     */
     public void update(String id, Property property) {
         Property propertyToUpdate = this.findById(id);
         if (nonNull(propertyToUpdate)) {
             propertyToUpdate.setIsFeatured(property.getIsFeatured());
             propertyToUpdate.setIsPromoted(property.getIsPromoted());
             propertyToUpdate.setPrice(property.getPrice());
+            propertyToUpdate.setCurrencyCode(property.getCurrencyCode());
+            propertyToUpdate.setAddress(property.getAddress());
+            propertyToUpdate.setCity(property.getCity());
+            propertyToUpdate.setArea(property.getArea());
+            propertyToUpdate.setBathroomCnt(property.getBathroomCnt());
+            propertyToUpdate.setBedroomCnt(property.getBedroomCnt());
+            propertyToUpdate.setDescription(property.getDescription());
+            propertyToUpdate.setPropertyAction(property.getPropertyAction());
         }
 
     }
